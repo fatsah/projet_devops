a760f38a86f4b4bb133d0f2c71507f95826d7e54
fatsah <tacherfiout.fatsah@gmail.com>
2021-11-25 15:10:52 +0100

    commit pour la partie 3

--- test_app.sh
+++ test_app.sh
@@ -2,16 +2,32 @@
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
-cp sample_app.py tempdir/.
+#cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r templates/* tempdir/static/.
+ 
+
 #Création du fichier Dockerfile 
 
+rm tempdir/Dockerfile
+cp test_app.py tempdir/
+echo "FROM python" >> tempdir/Dockerfile
+echo "RUN pip3 install flask" >> tempdir/Dockerfile
+echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY test_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/test_app.py" >> tempdir/Dockerfile
+
 #Lancement du build de l'image testapp à partir de Dockerfile sous /tempdir
+ 
+cd tempdir
+docker build -t testapp .
 
-
-#lancement du container  testrunning avec l'image testapp
-
+#lancement du container testrunning avec l'image testapp
+ 
+docker run -t -d -p 5050:5050 --name testrunning testapp
 
 #Tester si le container a bien démarré
 
+docker ps -a
